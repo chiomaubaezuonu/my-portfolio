e18fed28f1f051983a0e04cd3694899def5277ea
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2023-11-19 08:46:36 +0100

    Added background image to landing page file

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -8,35 +8,33 @@ import styles from './landing.module.css'
 const LandingPage = () => {
     return (
         <div className='flex justify-between px-14 bg-black h-screen'>
-            <div className='landing-page'>
+            <div className='landingPage'>
                 <m.h1
                     initial={{ opacity: 0 }}
-                    animate={{ y: -25, opacity: 1, scale: 1 }}
-                    transition={{ ease: "easeOut", duration: 0.5 }}
-                    className={`${styles.whiteText} ${styles.bigText}`}>Hello.
+                    animate={{ y: -25, opacity: 1 }}
+                    transition={{ ease: "easeOut", duration: 0.2 }}
+                    whileHover={{ x: 40 }}
+                    className={`${styles.firstText} ${styles.whiteText} ${styles.bigText}`}>
                 </m.h1>
                 <m.h1
                     initial={{ opacity: 0 }}
                     animate={{ y: -25, opacity: 1, scale: 1 }}
-                    transition={{ ease: "easeOut", delay: 0.5, duration: 0.5 }}
-                    className={styles.bigText}>I am
+                    whileHover={{ x: 40}}
+                    transition={{ ease: "easeOut", delay: 0.25, duration: 0.5 }}
+                    className={`${styles.secondText} ${styles.bigText}`}>
                 </m.h1>
                 <m.h1
                     initial={{ opacity: 0 }}
                     animate={{ y: -25, opacity: 1, scale: 1 }}
-                    transition={{ ease: "easeOut", delay: 1, duration: 1 }}
-                    className={styles.bigText}>Chioma
+                    transition={{ ease: "easeOut", delay: 0.5, duration: 1 }}
+                    whileHover={{ x: 40 }}
+                    className={`${styles.thirdText} ${styles.bigText}`}>
+
                 </m.h1>
             </div>
-            {/* <motion.div className='bg-orange-500 p-6 border-r-emerald-500'
-                animate={{ y: 100 }}
-                transition={{ ease: "easeIn", duration: 2 }}
-            >
-                Hello
-            </motion.div> */}
             <m.div animate={{ y: -20 }}
                 transition={{ ease: "easeIn", duration: 2 }}>
-                <Image src={me2} alt="my-profile" width={300} height={700} />
+                <Image className={styles.background} src={'/me2.jpeg'} alt="my-profile" width={300} height={700} />
             </m.div>
 
         </div>
