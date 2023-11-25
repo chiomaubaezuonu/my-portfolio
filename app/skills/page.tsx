972762dbe21ea8f652054189e310ff45e7175349
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2023-11-26 00:26:08 +0100

    Added styling to the skillcards and cards

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -12,29 +12,27 @@ type SkillsProps = {
   }[]
 }
 const Skills = (props: SkillsProps) => {
-  //const Skills = (props: PersonListProps) => {
   return (
-    <div className='bg-[#EFF4FB] p-8'>
-      <h1>My Skills</h1>
-      <div className='grid lg:grid-cols-4 gap-6 p-8'>
+    <div>
+
+      <div className={styles.skillCards}>
+        <h1 className={styles.skillTitle}>My Skills</h1>
         {props.mySkills.map(skill => {
           return (
-            <div className='bg-[#A8EFD4] m-auto px-16 py-8'
-              //  className={styles.skillCards}
-              key={skill.id}>
-              {/* <div className={styles.card}> */}
+            <div className={styles.card} key={skill.id}>
               {/* <Image src={skill.img} alt="html5" width={60} height={60} /> */}
               <img src={skill.img} alt="skillLogo" width={60} height={60} />
               <h2>{skill.level}</h2>
               <h2>{skill.skillName}</h2>
-              {/* </div> */}
-              <div></div>
             </div>
           )
         })}
       </div>
+      {/* </div> */}
     </div>
   )
 }
 
 export default Skills
+
+
