e922f6562f3114912592276f5481b4a395da5620
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2023-11-29 10:18:31 +0100

    Added data.ts file

---
+++ app/projects/data.ts
@@ -0,0 +1,18 @@
+export default [
+    {
+        id: 1,
+        img: '/images/todo.jpeg'
+    },
+    {
+        id: 1,
+        img: '/images/order.jpeg'
+    },
+    {
+        id: 1,
+        img: '/images/chatroom.jpeg'
+    },
+    {
+        id: 4,
+        img: '/images/metabnb.jpeg'
+    },
+]
