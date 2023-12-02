23542b77ca36f29e0254dc2133018c4bd6fd8562
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2023-12-02 02:56:42 +0100

    Added social media icons to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import styles from './contact.module.css'
+import Image from 'next/image'
+import github from './github.svg'
+import twitter from './twitter.svg'
+import linkedin from './linkedin.jpg'
+import phone from './phone.jpg'
+import mail from './email.png'
+
 
 const page = () => {
     return (
         <div className={styles.wrapper}>
-            <h1>Let's work together</h1>
-            <p>I am always open to new opportunities</p>
+            <h1 className={styles.title}>Let's work together</h1>
+            <p className={styles.text}>I am always open to new opportunities</p>
+            <div className={styles.socials}>
+                <Image src={github} alt='github' />
+                <Image src={twitter} alt='twitter' />
+                <Image src={linkedin} alt='Linkedin' width={50} height={50} />
+                <Image src={phone} style={{background: '#E8FAFB'}} alt='phone' width={50} height={50} />
+                <Image src={mail} style={{background: '#E8FAFB'}} alt='email' width={50} height={50} />
+            </div>
         </div>
     )
 }
