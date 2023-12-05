2c1910bc495c6f87466c59002fd876cd96a2f6de
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2023-12-05 16:46:45 +0100

    Added social media icons as links and added a modal

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,24 +1,43 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import styles from './contact.module.css'
 import Image from 'next/image'
 import github from './github.svg'
 import twitter from './twitter.svg'
-import linkedin from './linkedin.jpg'
-import phone from './phone.jpg'
-import mail from './email.png'
-
+import linkedin from './linkedin.svg'
+import phone from './phone.png'
+import mail from './email.svg'
+import Link from 'next/link'
 
 const page = () => {
+    const [contact, setContact] = useState<boolean>(false)
+    if (contact) {
+        console.log("yes")
+    }
     return (
         <div className={styles.wrapper}>
+            {contact && <div className={styles.modal}>
+                <form action="submit">
+                    <p className={styles.closeModal} onClick={() => setContact(false)}>X</p>
+                    <input type="text" placeholder='Name' />
+                    <textarea placeholder='Comment'></textarea>
+                </form>
+            </div>}
             <h1 className={styles.title}>Let's work together</h1>
             <p className={styles.text}>I am always open to new opportunities</p>
+            <button onClick={() => setContact(!contact)} className={styles.button}>Contact me</button>
             <div className={styles.socials}>
-                <Image src={github} alt='github' />
-                <Image src={twitter} alt='twitter' />
-                <Image src={linkedin} alt='Linkedin' width={50} height={50} />
-                <Image src={phone} style={{background: '#E8FAFB'}} alt='phone' width={50} height={50} />
-                <Image src={mail} style={{background: '#E8FAFB'}} alt='email' width={50} height={50} />
+                <Link href='https://github.com/chiomaubaezuonu'>
+                    <Image src={github} alt='github' />
+                </Link>
+                <Link href='https://www.linkedin.com/in/chioma-ubaezuonu-924b1214b/'>
+                    <Image src={linkedin} alt='Linkedin' />
+                </Link>
+                <Link href='https://twitter.com/ChiomaPresh1029'>
+                    <Image src={twitter} alt='twitter' />
+                </Link>
+                <Image src={phone} className={styles.phone} alt='phone' />
+                <Image src={mail} className={styles.mail} alt='email' />
             </div>
         </div>
     )
