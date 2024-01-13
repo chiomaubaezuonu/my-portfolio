2ac991ae5520d2474b532595d870a3345e1d92be
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-01-13 22:24:12 +0100

    styled inner overlay div

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -27,13 +27,10 @@ const Page = () => {
                             <div className={styles.overlay}>
                                 <h1 className={styles.projectName}>{project.title}</h1>
                             </div>
-                            {/* <div className={styles.innerOverlay}>
+                            <div className={styles.innerOverlay}>
                                 <p style={{ color: 'white' }}>{project.desc}</p>
                                 <a href="https://saveyourtodolist.netlify.app/"><button className={styles.websiteButton}>{project.link}</button></a>
                             </div>
-                            <div className={styles.content}>
-                                <button className={styles.websiteButton}>Visit Website</button>
-                            </div> */}
                         </div>
 
                     ))}
