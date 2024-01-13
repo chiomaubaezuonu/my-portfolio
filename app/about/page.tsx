77cddae0fb6f53bc42d15f18a2fb628699d1ac9d
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-01-13 08:54:31 +0100

    Change font size for project name

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,10 +2,8 @@
 import Image from 'next/image'
 import React from 'react'
 import hand from './handWave.png'
-//import handWave from '../ima'
 import { motion as m } from 'framer-motion'
 import styles from './about.module.css'
-// import '/global.css'
 
 const About = () => {
   return (
@@ -13,7 +11,7 @@ const About = () => {
       <div className={styles.aboutDiv}>
         <span className={styles.emoji}> Hi, I'm Chioma Precious 👋 </span>
         <p className={styles.about}>I am a skilled frontend web developer, I build exceptional and accessible digital experiences for the web.
-        My tech stack at work is Html5, Css, Javascript, Next.js, TypeScript, Firebase and SASS.
+          My tech stack at work is Html5, Css, Javascript, Next.js, TypeScript, Firebase and SASS.
         </p>
         {/* <p className={styles.about}></p> */}
       </div>
